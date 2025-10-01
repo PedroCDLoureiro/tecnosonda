fa936c8c3b28ca32c20aa5aa64a8bca61a560013
Pedro Loureiro <52416553+PedroCDLoureiro@users.noreply.github.com>
2025-10-01 16:00:33 -0300

    finish page sobre nós

--- wp-content/themes/tecnosonda/pages/sobre-nos.php
+++ wp-content/themes/tecnosonda/pages/sobre-nos.php
@@ -4,10 +4,10 @@
         'post_type' => 'sobre_nos',
         'posts_per_page' => 1,
     );
-    $query = new WP_Query($args);
+    $query_sobre_nos = new WP_Query($args);
     
-    if ($query->have_posts()) :
-        while ($query->have_posts()) : $query->the_post(); 
+    if ($query_sobre_nos->have_posts()) :
+        while ($query_sobre_nos->have_posts()) : $query_sobre_nos->the_post(); 
             $titulo_pagina = get_the_title();
             $subtitulo_banner = get_field('subtitulo_banner');
             $banner = get_field('banner');
@@ -34,7 +34,36 @@
        <?php endwhile;
         wp_reset_postdata();
     endif; ?>
-<?php get_template_part('content/nossos-servicos'); ?>
+    <?php get_template_part('content/nossos-servicos'); ?>
 
-<?php get_template_part('content/entre-em-contato'); ?>
+    <?php 
+        $query_sobre_nos->rewind_posts();
+        
+        if ($query_sobre_nos->have_posts()) :
+            while ($query_sobre_nos->have_posts()) : $query_sobre_nos->the_post();
+                
+                $midia = get_field('imagem_video_sobre_nos');
+            
+                if( $midia ): ?>
+                    <section id="video">
+                        <div class="container">
+                            <div class="midia">
+                                <?php if (strpos($midia['mime_type'], 'image') !== false): ?>
+                                    <img src="<?php echo esc_url($midia['url']); ?>" alt="<?php echo esc_attr($midia['alt']); ?>" class="img-fluid w-100" />
+                                <?php
+                                elseif (strpos($midia['mime_type'], 'video') !== false): ?>
+                                    <video id="video-nossos-valores" class="w-100">
+                                        <source src="<?php echo esc_url($midia['url']); ?>" type="<?php echo esc_attr($midia['mime_type']); ?>">
+                                        Seu navegador não suporta vídeos.
+                                    </video>
+                                    <button id="play-midia"></button>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </section>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+    <?php get_template_part('content/entre-em-contato'); ?>
 </section>
