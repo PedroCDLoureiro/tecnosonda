1f6f2e507d1eed22bcd21d20617be874f454a576
Pedro Loureiro <52416553+PedroCDLoureiro@users.noreply.github.com>
2025-10-08 15:06:39 -0300

    start page Orçamento

---
+++ wp-content/themes/tecnosonda/templates/orcamento.php
@@ -0,0 +1,26 @@
+<?php 
+    /**
+        * Template Name: Página Orçamento
+    */
+    get_header();
+?>
+<section id="page-orcamento" class="page">
+    <?php
+        $titulo_pagina = get_the_title();
+        $banner = get_field('banner');
+    ?>
+    <div id="banner" class="container-fluid">
+        <img src="<?= $banner; ?>" alt="<?= $titulo_pagina; ?>" class="w-100">
+        <div class="texts">
+            <h1><?= $titulo_pagina; ?></h1>
+        </div>
+    </div>
+    <div id="container-form" class="container-fluid container-text-image show-after">
+        <div class="container show-before">
+            <div class="col-md-8 offset-md-2 col-12 d-flex flex-column justify-content-center text-white sobre">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
